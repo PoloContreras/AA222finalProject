413db18a2a94cd4a92558d94fe4b794f10ebafde
NancyDiallo <33468807+NancyDiallo@users.noreply.github.com>
2023-06-05 12:18:12 -0700

    Update pop_evol.py

--- pop_evol.py
+++ pop_evol.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
+import multiprocessing
+
 from aa222_finalproject_regression import BuildModel, evaluateModel, datasetLoad
 from HookeJeeves import HookeJeevesPop
 from random import randint
@@ -13,22 +15,21 @@ from random import randint
 
 # max neurons is kinda arbitrary, so look into manipulating that maybe
 def generate_random_architecture(max_layers=5, max_neurons=64):
-   num_layers = random.randint(2, max_layers)
-   architecture = [28]  # Starting with input layer neurons
+    num_layers = random.randint(2, max_layers)
+    architecture = [28]  # Starting with input layer neurons
 
-   for i in range(num_layers - 1):
-       if i >=2 and architecture[i-1] == 0:
-        architecture.extend([0]*(num_layers -1-i))
-        break
-       architecture.append(random.randint(0, max_neurons))
+    for i in range(num_layers - 1):
+        if i >= 1 and architecture[i-1] == 0:
+            architecture.extend([0] * (num_layers - i))
+            break
+        architecture.append(random.randint(1, max_neurons))
 
-   if architecture[1] == 0 or (num_layers >= 3 and architecture[2] == 0):
+    if architecture[1] == 0 or (num_layers >= 3 and architecture[2] == 0):
         architecture[3:-1] = [0] * (len(architecture) - 4)
-  
-   architecture[-1] = 2
 
-   return tuple(architecture)
+    architecture[-1] = 2
 
+    return tuple(architecture)
 
 def generate_population(size):
     population = []
@@ -43,24 +44,27 @@ def generate_population(size):
 
 
 def evaluate_population(population, xTest, yTest):
-    scores = []
-    for architecture in population:
-        if has_zero_neurons_after_layer(architecture):
-            scores.append(0)  # Assign a score of 0 to architectures with invalid configurations
-        else:
-            model = BuildModel(architecture)
-            score = evaluateModel(model, architecture, xTest, yTest, verbose=False)
-            scores.append(score)
+    pool = multiprocessing.Pool()
+    scores = pool.map(evaluate_architecture, [(arch, xTest, yTest) for arch in population])
+    pool.close()
+    pool.join()
     return scores
+
+def evaluate_architecture(args):
+    architecture, xTest, yTest = args
+    if has_zero_neurons_after_layer(architecture):
+        return 0
+    else:
+        model = BuildModel(architecture)
+        score = evaluateModel(model, architecture, xTest, yTest, verbose=False)
+        return score
+
 
 def has_zero_neurons_after_layer(architecture):
     for i in range(1, len(architecture)):
         if architecture[i-1] == 0 and architecture[i] != 0:
             return True
     return False
-
-
-
 
 def evolve_population(population, scores, parents_max):
    sorted_indices = np.argsort(scores)
@@ -76,105 +80,116 @@ def evolve_population(population, scores, parents_max):
 
    return new_pop
 
-
 def mutate_architecture(architecture, num_mutations=1):
-   mutated_architecture = list(architecture)
-  
-   for _ in range(num_mutations):
-       # Generate a mutated architecture using Hooke-Jeeves
-       mutated_architecture_np = HookeJeevesPop(architecture)
-       mutated_architecture_tuple= tuple(mutated_architecture_np)
-
-       if mutated_architecture_tuple[0] == 0:
-            continue
+    mutated_architecture = list(architecture)
     
-       if mutated_architecture_tuple[1] == 0 or (len(mutated_architecture_tuple) >= 3 and mutated_architecture_tuple[2] == 0):
-            mutated_architecture_tuple = list(mutated_architecture_tuple)
-            mutated_architecture_tuple[3:-1] = [0] * (len(mutated_architecture_tuple) - 4)
-            mutated_architecture_tuple = tuple(mutated_architecture_tuple)
+    # Generate a mutated architecture using Hooke-Jeeves
+    mutated_architecture_np = HookeJeevesPop(architecture)
+    mutated_architecture_tuple = tuple(mutated_architecture_np)
+    
+    if mutated_architecture_tuple[0] == 0:
+        return architecture
+    
+    if mutated_architecture_tuple[1] == 0 or (len(mutated_architecture_tuple) >= 3 and mutated_architecture_tuple[2] == 0):
+        mutated_architecture_tuple = list(mutated_architecture_tuple)
+        mutated_architecture_tuple[3:-1] = [0] * (len(mutated_architecture_tuple) - 4)
+        mutated_architecture_tuple = tuple(mutated_architecture_tuple)
+    
+    # Apply additional mutations
+    for _ in range(num_mutations - 1):
+        # Randomly select a position in the architecture to mutate
+        mutation_index = random.randint(0, len(mutated_architecture) - 1)
+        
+        # Ensure a maximum of five layers
+        if mutation_index == 1:
+            mutated_architecture[mutation_index] = random.randint(1, 64)
+        elif mutation_index >= 2 and mutation_index < 6:
+            mutated_architecture[mutation_index] = random.randint(0, 64)
+        
+        # Ensure the last layer is equal to 2
+        if mutation_index == len(mutated_architecture) - 1:
+            mutated_architecture[mutation_index] = 2
+    
+    return tuple(mutated_architecture)
 
-       # Add the mutated architecture to the list
-       mutated_architecture.extend(mutated_architecture_tuple)
 
-
-   return tuple(mutated_architecture)
 
 
 # Main (Regression Dataset)
-x, y, xTest, yTest = datasetLoad()
-population_size = 50
-parents_max = 10
-generations_max = 10
+if __name__ == '__main__':
+    x, y, xTest, yTest = datasetLoad()
+    population_size = 10
+    parents_max = 5
+    generations_max = 5
+    # generations_max can definitely be increased fo’ sho’
 
+    population = generate_population(population_size)
 
-# generations_max can definitely be increased fo’ sho’
+    scores_history = []
+    generations = []
 
+    for generation in range(generations_max):
+        scores = evaluate_population(population, xTest, yTest)
+        best_arch = [population[i] for i in np.argsort(scores)[:parents_max]]
+        best_networks = []
 
-population = generate_population(population_size)
+        population = evolve_population(population, scores, parents_max)
 
+        scores_history.append(scores)
+        generations.append(generation)
 
-for generation in range(generations_max):
-   scores = evaluate_population(population, xTest, yTest)
-   best_arch = [population[i] for i in np.argsort(scores)[:parents_max]]
-   best_networks = []
+    # Get best architecture and best network
+    best_architecture = population[np.argmin(scores)]
+    best_network = BuildModel(best_architecture)
+    best_network_output = evaluateModel(best_network, best_architecture, xTest, yTest, verbose = True)
 
+    # Get the Ensemble Working
+    ensemble_outputs = []
+    for architecture in best_arch:
+        network = BuildModel(architecture)
+        ensemble_outputs.append(evaluateModel(network, architecture, xTest, yTest))
 
-   for architecture in best_arch:
-       best_networks.append(HookeJeevesPop(architecture))
+    ensemble_output = np.mean(ensemble_outputs)
+    # Create a DataFrame with architectures and scores
+    data = {
+        'Architecture': population,
+        'Score': scores
+    }
+    df = pd.DataFrame(data)
 
+    # Display the table
+    print(df)
 
-   population = evolve_population(population, scores, parents_max)
+  
+   
 
+    for i in range(len(scores_history)):
+        plt.plot(generations, scores_history[i], marker='o', label='Generation {}'.format(i + 1))
+      
+    plt.xlabel('Generation')
+    plt.ylabel('Score')
+    plt.title('Scores over Generations')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
-# Get best architecture and best network
-best_architecture = population[np.argmin(scores)]
-best_network = BuildModel(best_architecture)
-best_network_output = evaluateModel(best_network, best_architecture, xTest, yTest, verbose = True)
+    data = {
+        'Architecture': best_arch,
+        'Output': ensemble_outputs
+    }
+    df = pd.DataFrame(data)
 
-# Get the Ensemble Working
-ensemble_outputs = []
-for network, architecture in zip(best_networks, best_arch):
-    ensemble_outputs.append(evaluateModel(network, architecture, xTest, yTest))
+    # Display the table
+    print(df)
 
-ensemble_output = np.mean(ensemble_outputs)
-
-# Create a DataFrame with architectures and scores
-data = {
-    'Architecture': population,
-    'Score': scores
-}
-df = pd.DataFrame(data)
-
-# Display the table
-print(df)
-
-# Create a line plot
-generations = range(1, generations_max + 1)
-plt.plot(generations, scores, marker='o')
-plt.xlabel('Generation')
-plt.ylabel('Score')
-plt.title('Scores over Generations')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
-data = {
-    'Architecture': best_arch,
-    'Output': ensemble_outputs
-}
-df = pd.DataFrame(data)
-
-# Display the table
-print(df)
-
-# Create a box plot
-plt.boxplot(ensemble_outputs)
-plt.xlabel('Network')
-plt.ylabel('Output')
-plt.title('Ensemble Outputs')
-plt.xticks(range(1, len(best_arch) + 1), best_arch, rotation=90)
-plt.tight_layout()
-plt.show()
+    # Create a box plot
+    plt.boxplot(ensemble_outputs)
+    plt.xlabel('Network')
+    plt.ylabel('Output')
+    plt.title('Ensemble Outputs')
+    plt.xticks(range(1, len(best_arch) + 1), best_arch, rotation=90)
+    plt.tight_layout()
+    plt.show()
 
 # —--------------------------------------------------
 # **Potential code for best initial guess before hooke-Jeeves implementation**
